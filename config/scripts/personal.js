2e9ae6db042da6578ac14c0c096617bcfa5c6650
dylan <dylan@belowaverage.org>
2017-10-07 10:05:31 -0700

    Working with personal and share
    Signed-off-by: dylan <dylan@belowaverage.org>

--- config/scripts/personal.js
+++ config/scripts/personal.js
@@ -1,3 +1,3 @@
 $('#li').click(function() {
-	window.location = 'https://'+$('input[name=username]').val()+':'+$('input[name=password]').val()+'@personal.belowaverage.org/'+$('input[name=username]').val()+'/';
+	window.location = 'https://'+$('input[name=username]').val()+':'+$('input[name=password]').val()+'@personal.belowaverage.org/'+$('input[name=username]').val()+'/?b';
 });
--- config/scripts/share.js
+++ config/scripts/share.js
@@ -1,3 +1,3 @@
 $('#li').click(function() {
-	window.location = 'https://'+$('input[name=username]').val()+':'+$('input[name=password]').val()+'@share.belowaverage.org/';
+	window.location = 'https://'+$('input[name=username]').val()+':'+$('input[name=password]').val()+'@share.belowaverage.org/?b';
 });
