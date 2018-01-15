0059475804360f61a617676194d956e067bdf43b
Dylan Bickerstaff <dylan@belowaverage.org>
2018-01-14 22:19:38 -0500

    Finished socket auth

--- config/scripts/websocket.js
+++ config/scripts/websocket.js
@@ -1,17 +1,30 @@
 $(document).ready(function() {
-	if(document.location.hostname == 'login.belowaverage.org' && parent == window) {
+	$.getJSON('config/scripts/websocket/trusted.json', function(data) {
+		$.each(data, function() {
+			if(this+'' == document.referrer+'') {
+				start();
+				return false;
+			}
+		});
+	});
+	function start() {
 		var retryCount = 0;
+		var isError = true;
+		var ws = null;
 		function connect() {
+			retryCount++;
 			fadeOn();
 			$('#progress').text('Connecting...');
-			var ws = new WebSocket('wss://api.belowaverage.org/v2/message');
+			ws = new WebSocket('wss://api.belowaverage.org/v2/message');
 			ws.onopen = function() {
 				ws.send('join '+hash.split('#')[0]);
 				fadeOff();
 				$('#progress').text('');
 			};
 			ws.onmessage = function() {
+				retryCount = 10;
 				ws.close();
+				isError = false;
 				$('#error').text('Session compromised... Connection closed.');
 				$('#box *').attr('disabled', 'true');
 				fadeOff();
@@ -22,27 +35,21 @@ $(document).ready(function() {
 					$('#error').text('No login session found. Please try again.');
 					$('#box *').attr('disabled', 'true');
 				} else {
-					if(retryCount <= 10) {
+					if(retryCount < 10) {
 						fadeOn();
 						$('#progress').text('Attempt: '+retryCount);
 						setTimeout(function() {
-							retryCount++;
 							connect();
 						}, 100);
 					} else {
 						fadeOff();
-						$('#error').text('Failed to connect to server.');
+						if(isError) {
+							$('#error').text('Failed to connect to server.');
+						}
 						$('#box *').attr('disabled', 'true');
 					}
 				}
 			};
-		};
-		function send_auth(AUTH) {
-			localStorage.setItem('AUTH', AUTH);
-			$.post('https://api.belowaverage.org/v1/message/', {
-				id: hash.split('#')[0],
-				message: AUTH
-			});
 		};
 		if(location.hash.length == 33) {
 			connect();
@@ -58,17 +65,17 @@ $(document).ready(function() {
 				username: $('input[name=username]').val(),
 				password: $('input[name=password]').val()
 			}, function(AUTH) {
-				send_auth(AUTH);
+				if(ws.OPEN) {
+					isError = false;
+					retryCount = 10;
+					ws.send(AUTH);
+					ws.close();
+					$('#error').text('');
+				}
 			}).fail(function(e) {
 				$('#error').text(e.responseText);
 				fadeOff();
 			});
 		});
-	} else if(document.location.hostname == 'login.belowaverage.org' && parent !== window) {
-		$('#error').text('WARNING: This login may be compromised.');
-		$('#box *').attr('disabled', 'true');
-	} else {
-		$('#error').text('WARNING: "'+document.location.hostname+'" domain not trusted!');
-		$('#box *').attr('disabled', 'true');
 	}
 });
