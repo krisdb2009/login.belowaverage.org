fa6e691987d0341365ac5143f9dc9a6177fa7164
Dylan Bickerstaff <dylan@belowaverage.org>
2018-12-03 22:04:08 -0500

    Consolidated and added raw cred support.

--- config/scripts/webdows.js
+++
@@ -1,3 +0,0 @@
-if(document.location.hostname == 'webdows.belowaverage.org' && window.frameElement == null) {
-	window.location = 'https://webdows.belowaverage.org/';
-}
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
 var hash = window.location.hash.substr(1);
 var username = '';
 var password = '';
+var ran = false;
 var url = location.origin + location.pathname;
+var origin = null;
+var trust = {}; 
 if(hash.split(':').length == 2 && hash.split('@').length == 2) {
 	username = hash.split(':')[0].split('@')[1];
 	password = hash.split(':')[1];
@@ -12,25 +15,59 @@ function fadeOn() {
 function fadeOff() {
 	$('#body, #loader, #footer').removeClass('fade');
 }
-$(document).ready(function() {
-	function ready() {
-		if(username !== '' && password !== '') {
-			$('input[name=username]').val(username);
-			$('input[name=password]').val(password);
-			setInterval(function() {
-				$('#li').click();
-			}, 100);
+function encrypt(AUTH, credential, callback) {
+	$.post('https://api.belowaverage.org/v1/sso/', {AUTH: AUTH}, function(data) {
+		if(data.control !== 'encrypt') {
+			encrypt(AUTH, credential, callback);
+		} else if(data.control == 'encrypt') {
+			localStorage.AUTH_Crypt = sjcl.encrypt(data.key, JSON.stringify(credential));
+			callback.call();
+		}
+	}, 'json');
+}
+function decrypt(AUTH, callback) {
+	$.post('https://api.belowaverage.org/v1/sso/', {AUTH: AUTH}, function(data) {
+		if(data.control == 'decrypt') {
+			callback.call(JSON.parse(sjcl.decrypt(data.key, localStorage.AUTH_Crypt)));
 		} else {
+			callback.call(false);
+		}
+	}, 'json');
+}
+function showForm(e) {
+	fadeOff();
+	$('#progress').text('');
+	$('#li').click(function() {
+		$('#progress').text('Authenticating...');
+		var credential = {
+			username: $('input[name=username]').val(),
+			password: $('input[name=password]').val()
+		};
+		$.post('https://api.belowaverage.org/v1/AUTH/', credential, function(AUTH) {
+			localStorage.AUTH_Token = AUTH;
+			$('#progress').text('Encrypting credentials...');
+			encrypt(AUTH, credential, function() {
+				passCredOrToken();
+			});
+		}).fail(function(e) {
+			$('#error').text(e.responseText);
 			fadeOff();
-		}
-	};
-	$.getJSON('config/config.json', function(data) {
-		if(typeof data[url] == 'string') {
-			$.get('config/scripts/'+data[url]+'.js', ready);
-		} else {
-			ready();
-		}
+		});
 	});
+}
+function passCredOrToken() {
+	if(trust[origin] == 'token') { //If credential type is token.
+		parent.postMessage(localStorage.AUTH_Token, origin); //Pass token to parent.
+	} else if(trust[origin] == 'crypt') { //If credential type is crypt.
+		decrypt(localStorage.AUTH_Token, function() {
+			var credential = this;
+			encrypt(localStorage.AUTH_Token, credential, function() {
+				parent.postMessage(credential, origin); //Pass credential to parent.
+			});
+		});
+	}
+}
+$(document).ready(function() {
 	$('#li').click(function() {
 		fadeOn();
 	});
@@ -38,5 +75,36 @@ $(document).ready(function() {
 		if(e.keyCode == 13) {
 			$('#li').click();
 		}
+	});
+	$('#progress').text('Verifying login trust...');
+	$.getJSON('config/login.json', function(trusted) {
+		onmessage = function(e) {
+			origin = e.origin;
+			trust = trusted.trust;
+			if(typeof trust[origin] !== 'undefined' && !ran) {
+				ran = true;
+				parent.postMessage('ready', origin);
+				if(window.location.hash == '#logout') {
+					$.post('https://api.belowaverage.org/v1/AUTH/', {AUTH: localStorage.AUTH_Token, logout: true});
+					localStorage.AUTH_Token = '';
+				}
+				if(typeof localStorage.AUTH_Token == 'string' && localStorage.AUTH_Token.length == 32) { //If local token found.
+					$('#progress').text('Authenticating...');
+					$.post('https://api.belowaverage.org/v1/AUTH/', {AUTH: localStorage.AUTH_Token}, function() { //Validate token.
+						passCredOrToken();
+					}).fail(function() { //If token is invalid.
+						showForm(e); //Prompt for new credentials.
+					});
+				} else { //If local token is not found.
+					showForm(e); //Prompt for credentials.
+				}
+			} else if(!ran) {
+				ran = true;
+				$('#progress').text('');
+				$('#error').html('<b>WARNING</b>: This origin ('+origin+') is not authorized to perform login requests.');
+				$('input[name=username], input[name=password], #li').attr('disabled', true);
+				fadeOff();
+			}
+		};
 	});
 });
