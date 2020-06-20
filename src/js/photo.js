186964626a0916e683ecf3cbc394d69d108a83ad
Dylan Bickerstaff <dylan@belowaverage.org>
2020-06-20 00:22:17 -0400

    Changed picture stuff

--- src/js/photo.js
+++ src/js/photo.js
@@ -2,7 +2,18 @@ window.addEventListener("load", function(e) {
     var image = document.querySelector(".login-card .picture");
     var username = document.querySelector("input[name=username]");
     username.onchange = function() {
-        console.log(username.value);
-        image.style.backgroundImage = "url('https://api.belowaverage.org/v2/adphoto/?" + username.value + "')";
+        xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.belowaverage.org/adphoto/?' + username.value + '');
+        xhr.responseType = 'blob';
+        xhr.onload = function() {
+            conType = xhr.getResponseHeader('Content-Type');
+            if(!conType.includes('text')) {
+                img = URL.createObjectURL(xhr.response);
+                image.style.backgroundImage = "url('" + img + "')";
+            } else {
+                image.style.backgroundImage = '';
+            }
+        };
+        xhr.send();
     };
 });
